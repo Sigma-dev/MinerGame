efc0731be647650711a06d77cc9e443a8989aca1
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-16 10:41:09 +0200

    Progress on the ressource spawning

---
+++ scripts/Player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -350.0
+var mining = false
+var facing_right = true
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite2D
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func updateAnimation():
+	if facing_right:
+		sprite.flip_h = false
+	else:
+		sprite.flip_h = true
+	var anim = "idle"
+	if abs(velocity.x) > 0:
+		anim = "run"
+	if mining:
+		anim = "mine"
+	if !is_on_floor():
+		anim = "jump"
+	animation_player.play(anim)
+
+func endMining():
+	mining = false
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("run_left", "run_right")
+	if direction:
+		if (direction > 0):
+			facing_right = true
+		else:
+			facing_right = false
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("click") and is_on_floor():
+		mining = true
+	updateAnimation()
---
+++ scripts/TileSpawner.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+@export var to_spawn: PackedScene
+@export var spawn_protection: PackedScene
+@onready var tilemap: TileMap = get_parent()
+
+func get_ground_tiles():
+	var coords = [
+		Vector2i(1, 2),
+		Vector2i(2, 1),
+		Vector2i(3, 1),
+		Vector2i(4, 1),
+		Vector2i(5, 1),
+		Vector2i(8, 1),
+		Vector2i(6, 2),
+		Vector2i(8, 3),
+		Vector2i(9, 3),
+		Vector2i(10, 3)
+	]
+	var tiles = []
+	for coord in coords:
+		tiles += tilemap.get_used_cells_by_id(0, 0, coord)
+	return tiles
+
+func spawn_on_random_ground():
+	var candidates = get_ground_tiles()
+	var iter = 0
+	while iter < 30:
+		iter += 1
+		var tile = candidates[randi() % candidates.size()]
+		var ontop_pos = Vector2i(tile.x, tile.y - 1)
+		if (tilemap.get_cell_tile_data(0, ontop_pos) != null):
+			continue
+		var spawn_pos = to_global((tilemap.map_to_local(tile) + tilemap.map_to_local(ontop_pos)) / 2)
+		spawn_at_pos(spawn_pos, spawn_protection)
+		break
+	#get the position of the top side of the selected tile by dividing the position of the tile on top of it with  map_to_local
+	#spawn object on the top side coordinates
+
+func spawn_at_pos(pos, object = to_spawn, ):
+	var instance = object.instantiate()
+	instance.global_position = pos
+	get_tree().root.get_child(0).add_child(instance);
+	if instance.has_method("set_manager"):
+		instance.set_manager(self)
+
+func _ready():
+	get_ground_tiles()# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if to_spawn && Input.is_action_pressed("click"):
+		print("jump")
+		spawn_on_random_ground()
+	pass
