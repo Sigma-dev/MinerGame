a2f40c29184f1ce50ee996a1690f40c0f916984c
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-17 11:48:18 +0200

    Better crawling

--- scenes/Crawler2.gd
+++ scenes/Crawler2.gd
@@ -12,7 +12,7 @@ var touched_ground_again = false
 
 static func compare_floats(a, b, epsilon = FLOAT_EPSILON):
 	var diff =  abs(a - b)
-	#print(diff)
+	print(diff)
 	return diff <= epsilon
 
 func raycast(origin, target):
@@ -36,22 +36,20 @@ func _physics_process(delta):
 	if rotating != 0:
 		var quat = Quaternion.from_euler(Vector3(0, 0, deg_to_rad(rotation_degrees)))
 		var target = Quaternion.from_euler(Vector3(0, 0, deg_to_rad(current + (90 * rotating))))
-		rotation_degrees = rad_to_deg(quat.slerp(target, 0.3).get_euler().z)
+		rotation_degrees = rad_to_deg(quat.slerp(target, 0.5).get_euler().z)
 		print("start")
 		print(rotating)
-		if (rotation_degrees < 0):
-			rotation_degrees += 360
 		#rotation_degrees = lerp(rotation_degrees, current + 90, 0.3)
-		var rot = fmod(rotation_degrees, 359.9999)
-		var t_rot = fmod(current + (90.0 * rotating), 359.9999)
-		#print (rot)
-		#print( t_rot)
+		var rot = fmod(float(int(round(rotation_degrees))), 360)
+		var t_rot = fmod(current + (90.0 * rotating), 360)
+		print (rot)
+		print( t_rot)
 		if compare_floats(fmod(rot - t_rot, 360), 0.0):
 			touched_ground_again = false
 			current += 90.0 * rotating
 			rotating = 0
 		return
-	velocity = transform.x * 100
+	velocity = transform.x * 200
 	
 	move_and_slide()
 
