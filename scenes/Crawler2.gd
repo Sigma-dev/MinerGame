f32f4f266293d3b61cb5aaaa963a8ced85af6f58
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-17 11:34:04 +0200

    Working crawler, takes corners too wide

--- scenes/Crawler2.gd
+++ scenes/Crawler2.gd
@@ -4,14 +4,15 @@ const FLOAT_EPSILON = 0.01
 
 @onready var shape = $CollisionShape2D
 @onready var ray_bot: RayCast2D = $RayBot
+@onready var ray_right: RayCast2D = $RayRight
 var settled = false
-var rotating = false
+var rotating = 0
 var current = 0.0
 var touched_ground_again = false
 
 static func compare_floats(a, b, epsilon = FLOAT_EPSILON):
 	var diff =  abs(a - b)
-	print(diff)
+	#print(diff)
 	return diff <= epsilon
 
 func raycast(origin, target):
@@ -19,32 +20,36 @@ func raycast(origin, target):
 	var query = PhysicsRayQueryParameters2D.create(origin, target)
 	return space_state.intersect_ray(query)
 
-func _physics_process(delta):
+func wait_settle():
 	if !settled && ray_bot.get_collider():
 		settled = true
+	return settled
+func _physics_process(delta):
+	if !wait_settle():
+		return
 	if ray_bot.get_collider():
 		touched_ground_again = true
-	if !settled:
-		return
-	if !ray_bot.get_collider() && touched_ground_again:
-		rotating = true
-	if rotating:
+	if !ray_bot.get_collider() && touched_ground_again && rotating == 0:
+		rotating = 1
+	if ray_right.get_collider() && touched_ground_again && rotating == 0:
+		rotating = -1
+	if rotating != 0:
 		var quat = Quaternion.from_euler(Vector3(0, 0, deg_to_rad(rotation_degrees)))
-		var target = Quaternion.from_euler(Vector3(0, 0, deg_to_rad(current + 90)))
+		var target = Quaternion.from_euler(Vector3(0, 0, deg_to_rad(current + (90 * rotating))))
 		rotation_degrees = rad_to_deg(quat.slerp(target, 0.3).get_euler().z)
 		print("start")
-
+		print(rotating)
 		if (rotation_degrees < 0):
 			rotation_degrees += 360
 		#rotation_degrees = lerp(rotation_degrees, current + 90, 0.3)
 		var rot = fmod(rotation_degrees, 359.9999)
-		var t_rot = fmod(current + 90.0, 359.9999)
-		print (rot)
-		print( t_rot)
-		if compare_floats(rot, t_rot):
+		var t_rot = fmod(current + (90.0 * rotating), 359.9999)
+		#print (rot)
+		#print( t_rot)
+		if compare_floats(fmod(rot - t_rot, 360), 0.0):
 			touched_ground_again = false
-			current += 90.0
-			rotating = false
+			current += 90.0 * rotating
+			rotating = 0
 		return
 	velocity = transform.x * 100
 	
