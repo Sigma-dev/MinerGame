874a10a39b6dfd7b428335781a148735a26c199a
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-19 12:24:47 +0200

    add only affect player to hitoncontact

--- addons/plugins/Components/HitOnContact2D/HitOnContact2D.gd
+++ addons/plugins/Components/HitOnContact2D/HitOnContact2D.gd
@@ -3,11 +3,15 @@ class_name HitOnContact2D
 extends Node2D
 
 @export var damage = 10
+@export var only_affect_player = false
 
 func _physics_process(delta):
 	var count = get_parent().get_slide_collision_count() 
 	for i in count:
 		var hit_info = get_parent().get_slide_collision(i)
-		for child in hit_info.get_collider().get_children():
+		var hit = hit_info.get_collider()
+		for child in hit.get_children():
 			if child is Hittable:
+				if only_affect_player && !hit.get_groups().has("Player"):
+					continue
 				child.try_hit(damage, hit_info.get_position())
