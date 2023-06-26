c3a772fbd610fc41ad685e58afefa850fc2753d1
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-26 15:58:33 +0200

    More cleanup

--- scripts/world/zone_spawner.gd
+++ scripts/world/zone_spawner.gd
@@ -5,22 +5,15 @@ extends Node2D
 @export var zone : PackedScene = null
 var coords = Vector2i(0, 0)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	coords = Vector2i(int(global_position.x) / 512, int(global_position.y) / 276)
 	get_parent().add_zone_spawner(self, coords)
 	if start_spawned || Engine.is_editor_hint():
 		get_parent().deploy_zone_at(coords)
-	pass # Replace with function body.
 
 
 func deploy():
 	var instance = zone.instantiate()
 	add_child(instance);
 	instance.global_position = global_position
-	#get_parent().add_zone(instance)
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
