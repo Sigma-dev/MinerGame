1feef3c8e84ef818d8e9d771275a0c6ee7be5829
Sigma-dev <72008323+Sigma-dev@users.noreply.github.com>
2023-06-30 18:31:48 +0200

    Added object selection in inventory

--- scripts/ui/Inventory.gd
+++ scripts/ui/Inventory.gd
@@ -1,9 +1,14 @@
-extends PanelContainer
+extends Control
 
 var slot_scene : PackedScene = preload("res://scenes/ui/inventory/inventory_slot.tscn")
 @export var inventory_data: InventoryData = null
-@onready var slots = $MarginContainer/Slots
+@onready var slots = $HBoxContainer/PanelContainer/MarginContainer/Slots
+@onready var item_texture = $HBoxContainer/SlotInfo/MarginContainer2/PanelContainer/Container/ItemTexture
+@onready var item_name = $HBoxContainer/SlotInfo/MarginContainer2/PanelContainer/Container/ItemName
+@onready var action_button = $HBoxContainer/SlotInfo/MarginContainer2/PanelContainer/Container/ActionButton
 
+var selected_slot : SlotData = null
+@onready var slot_info = $HBoxContainer/SlotInfo
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,6 +29,16 @@ func _update():
 	for slot_data in inventory_data.slots:
 		var instance = slot_scene.instantiate()
 		slots.add_child(instance)
+		instance.on_slot_selected.connect(on_slot_selected)
 		if slot_data:
 			instance.set_data(slot_data)
+	slot_info.visible = selected_slot != null
+	if selected_slot:
+		item_texture.texture = selected_slot.item_data.texture
+		item_name.text = selected_slot.item_data.name
+		action_button.visible = selected_slot.item_data.construction != null
+			
+func on_slot_selected(slot_data: SlotData):
+	selected_slot = slot_data
+	_update()
 
